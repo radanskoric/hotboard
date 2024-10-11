0117facf81c63bb885efe0c6cc38eeb26045fa24
Radan Skorić <radan.skoric@gmail.com>
2024-10-11 14:04:32 +0200

    [broadcasting] Make the board collaborative by turning on broadcasting

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,7 @@
 class Ticket < ApplicationRecord
   validates_presence_of :title, :description
+
+  after_update_commit -> { broadcast_refresh_later_to("main-board") }
+  after_create_commit  -> { broadcast_refresh_later_to("main-board") }
+  after_destroy_commit -> { broadcast_refresh_later_to("main-board") }
 end
