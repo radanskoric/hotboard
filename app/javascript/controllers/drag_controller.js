73889157b4aec6fbcd460dd110aa52e434726987
Radan Skorić <radan.skoric@gmail.com>
2024-10-11 15:10:58 +0200

    [drag-events-1] Set up drag dropzone callbacks

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -2,4 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["draggable", "dropzone"]
+
+  dropzoneTargetConnected(element) {
+    element.addEventListener("dragover", (event) => event.preventDefault())
+    element.addEventListener("drop", this.drop)
+  }
+
+  drop = () => {
+    console.log("DROPPED") // We'll implement this later.
+  }
 }
