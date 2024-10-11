604858b1ec8f1dbadbb705383b45a484a183280d
Radan Skorić <radan.skoric@gmail.com>
2024-10-11 14:54:00 +0200

    [add-state] Add state and change the rendering to kanban board columns

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ApplicationRecord
+  enum :state, %i[backlog next in_progress done], default: :backlog
+
   validates_presence_of :title, :description
 
   after_update_commit -> { broadcast_refresh_later_to("main-board") }
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_07_084948) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_11_123901) do
   create_table "tickets", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "state", default: 0, null: false
   end
 end
