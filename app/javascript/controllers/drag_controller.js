c09ae83b6092352d4ea1fff727546c557c5b4c34
Radan Skorić <radan.skoric@gmail.com>
2024-10-11 15:27:44 +0200

    [finish-dragging] Implement the dragging update to finish ticket dragging

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -8,11 +8,27 @@ export default class extends Controller {
     element.addEventListener("drop", this.drop)
   }
 
-  start(_event) {
-    console.log("Started dragging")
+  start(event) {
+    // We store the url to which we'll have to submit the state change.
+    event.dataTransfer.setData("changeStateUrl", event.currentTarget.dataset.changeStateUrl)
   }
 
-  drop = () => {
-    console.log("DROPPED") // We'll implement this later.
+  drop = (event) => {
+    event.preventDefault()
+    // We get the url back out.
+    const changeStateUrl = event.dataTransfer.getData("changeStateUrl")
+    // And extract the new state from the drop target.
+    const state = event.currentTarget.dataset.ticketState
+
+    // Create an invisible turbo-powered link
+    const link = document.createElement('a')
+    link.setAttribute('href', `${changeStateUrl}?state=${state}`)
+    link.setAttribute('data-turbo-method', 'post')
+    link.style.display = 'none'
+    document.body.appendChild(link)
+
+    // Click the link and remove it from the DOM
+    link.click()
+    link.remove()
   }
 }
