566d5419fca8e2645d9cc88a33595fc81215b48a
Radan Skorić <radan.skoric@gmail.com>
2024-10-18 10:51:41 +0200

    [inline-new-refresh] Change inline new creation to use the refresh action

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -25,10 +25,10 @@ class TicketsController < ApplicationController
     @ticket = Ticket.new(ticket_params)
 
     if @ticket.save
-      @notice = "Ticket was successfully created."
+      flash.notice = "Ticket was successfully created."
       respond_to do |format|
         format.turbo_stream
-        format.html { redirect_to @ticket, notice: @notice }
+        format.html { redirect_to @ticket }
       end
     else
       render :new, status: :unprocessable_entity
