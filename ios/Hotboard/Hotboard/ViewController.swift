0e77c1a0e4517398c44065d83e01553181a7c23c
Radan Skorić <radan.skoric@gmail.com>
2025-01-24 14:06:25 +0100

    [ios-start] Generate a new iOS application

---
+++ ios/Hotboard/Hotboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotboard
+//
+//  Created by Radan on 24.01.2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
