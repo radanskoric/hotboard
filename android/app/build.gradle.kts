8aefffd16cf9e7b9326d7ec5753dd8cfe4188204
Radan Skorić <radan.skoric@gmail.com>
2025-01-24 15:26:43 +0100

    [android-native-start] Initialise a Hotwire Native Android application.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,21 +27,22 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("dev.hotwire:core:1.2.3")
+    implementation("dev.hotwire:navigation-fragments:1.2.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
