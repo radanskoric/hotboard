c7fb4a938106b17cf1ed9f965325d3a596ed7e19
Radan Skorić <radan.skoric@gmail.com>
2025-02-28 11:33:13 +0100

    [ios-dont-match-query] Do not match query string when testing path configuration patterns

--- ios/Hotboard/Hotboard/AppDelegate.swift
+++ ios/Hotboard/Hotboard/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions:
         [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        Hotwire.config.pathConfiguration.matchQueryStrings = false
         Hotwire.loadPathConfiguration(from: [
             .server(rootURL.appending(path: "configurations/ios_v1.json"))
         ])
