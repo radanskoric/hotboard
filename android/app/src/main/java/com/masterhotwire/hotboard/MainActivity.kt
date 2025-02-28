30a88e8016b2fa5d641471d64e4f5af87d0e7075
Radan Skorić <radan.skoric@gmail.com>
2025-02-28 14:00:14 +0100

    [android-load-path-config] Android: Load the path configuration from the server

--- android/app/src/main/java/com/masterhotwire/hotboard/MainActivity.kt
+++ android/app/src/main/java/com/masterhotwire/hotboard/MainActivity.kt
@@ -3,6 +3,8 @@ package com.masterhotwire.hotboard
 import android.os.Bundle
 import dev.hotwire.navigation.activities.HotwireActivity
 import dev.hotwire.navigation.navigator.NavigatorConfiguration
+import dev.hotwire.core.config.Hotwire
+import dev.hotwire.core.turbo.config.PathConfiguration
 
 const val baseURL = "http://10.0.2.2:3000"
 
@@ -10,6 +12,13 @@ class MainActivity : HotwireActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Hotwire.loadPathConfiguration(
+            context = this,
+            location = PathConfiguration.Location(
+                remoteFileUrl = "$baseURL/configurations/android_v1.json"
+            )
+        )
     }
 
     override fun navigatorConfigurations() = listOf(
