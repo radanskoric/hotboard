4c942004f0ce33f680d3e2cf85cef9b2c5c43622
Radan Skorić <radan.skoric@gmail.com>
2025-03-14 14:18:32 +0100

    [connect-submit-button-controller] Implement the web side of the submit button bridge component

--- app/javascript/controllers/bridge/submit_button_controller.js
+++ app/javascript/controllers/bridge/submit_button_controller.js
@@ -2,4 +2,14 @@ import { BridgeComponent } from "@hotwired/hotwire-native-bridge"
 
 export default class extends BridgeComponent {
   static component = "submit-button"
+
+  connect() {
+    this.send("connect", {title: this.bridgeElement.title}, () => {
+      this.bridgeElement.click()
+    })
+  }
+
+  disconnect() {
+    this.send("disconnect")
+  }
 }
