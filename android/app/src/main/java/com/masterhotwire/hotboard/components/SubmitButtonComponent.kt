bd0a1201ed581f35fc5c9163e607b695691b3ea5
Radan Skorić <radan.skoric@gmail.com>
2025-03-21 17:07:54 +0100

    [android-create-submit-button] Create the Android Submit Button Bridge components

---
+++ android/app/src/main/java/com/masterhotwire/hotboard/components/SubmitButtonComponent.kt
@@ -0,0 +1,65 @@
+package com.masterhotwire.hotboard.components
+
+import android.util.Log
+import android.view.Gravity
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import dev.hotwire.core.bridge.BridgeComponent
+import dev.hotwire.core.bridge.BridgeDelegate
+import dev.hotwire.core.bridge.Message
+import dev.hotwire.navigation.destinations.HotwireDestination
+import dev.hotwire.navigation.fragments.HotwireFragment
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+class SubmitButtonComponent(
+    name: String,
+    private val bridgeDelegate: BridgeDelegate<HotwireDestination>
+) : BridgeComponent<HotwireDestination>(name, bridgeDelegate) {
+    private val fragment: HotwireFragment
+        get() = bridgeDelegate.destination.fragment as HotwireFragment
+
+    override fun onReceive(message: Message) {
+        // Handle incoming messages based on the message `event`.
+        when (message.event) {
+            "connect" -> addButton(message)
+            else -> Log.w(
+                "ButtonComponent",
+                "Unknown event for message: $message"
+            )
+        }
+    }
+
+    private fun addButton(message: Message) {
+        // Deserialize the data payload we sent
+        // from the submit button controller
+        val data = message.data<MessageData>() ?: return
+
+        // Create the view which will render the button
+        val button = ComposeView(fragment.requireContext()).apply {
+            setContent {
+                Text(data.title)
+            }
+        }
+        // Create a layout that will put the button on
+        // the right side of the toolbar
+        val layoutParams = Toolbar.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        ).apply { gravity = Gravity.END } // END pushes it to the right side
+
+        // Fetch the toolbar
+        val toolbar = fragment.toolbarForNavigation()
+        // Add the new button
+        toolbar?.addView(button, layoutParams)
+    }
+
+    // Use kotlinx.serialization annotations to define a serializable
+    // data class that represents the incoming message.data json.
+    @Serializable
+    data class MessageData(
+        @SerialName("title") val title: String
+    )
+}
