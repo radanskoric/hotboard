2eff7abf91df34a2a08a02d25d61a141a0febb82
Radan Skorić <radan.skoric@gmail.com>
2025-04-14 11:11:42 +0200

    [cancel-button-component] Implement the ticket form cancel button as a native button

--- android/app/src/main/java/com/masterhotwire/hotboard/HotboardApplication.kt
+++ android/app/src/main/java/com/masterhotwire/hotboard/HotboardApplication.kt
@@ -1,6 +1,7 @@
 package com.masterhotwire.hotboard
 
 import android.app.Application
+import com.masterhotwire.hotboard.components.CancelButtonComponent
 import com.masterhotwire.hotboard.components.SubmitButtonComponent
 import dev.hotwire.core.bridge.BridgeComponentFactory
 import dev.hotwire.core.bridge.KotlinXJsonConverter
@@ -20,7 +21,8 @@ class HotboardApplication : Application() {
         )
 
         Hotwire.registerBridgeComponents(
-            BridgeComponentFactory("submit-button", ::SubmitButtonComponent)
+            BridgeComponentFactory("submit-button", ::SubmitButtonComponent),
+            BridgeComponentFactory("cancel-button", ::CancelButtonComponent)
         )
 
         Hotwire.config.jsonConverter = KotlinXJsonConverter()
